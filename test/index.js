53493591a0f73b2573960ca6117fbaede4c9f169
Wolfgang <wolf@okwolf.com>
2016-08-07 09:37:53 -0700

    test code cleanup

--- test/index.js
+++ test/index.js
@@ -3,71 +3,71 @@ import Erux from '../src';
 
 describe('Erux', () => {
   const reducerError = 'reducer to be a function';
+  const { reducer, enhancer, on } = Erux;
   describe('reducer function to get a reducer', () => {
     it('should be a function', () => {
-      assert.isFunction(Erux.reducer);
+      assert.isFunction(reducer);
     });
     it('should take the correct number of arguments', () => {
-      assert.strictEqual(Erux.reducer.length, 2);
+      assert.strictEqual(reducer.length, 2);
     });
     it('should return identical state without any reducers', () => {
       const initialState = {};
-      assert.strictEqual(Erux.reducer(initialState, {}), initialState);
+      assert.strictEqual(reducer(initialState, {}), initialState);
     });
   });
 
   describe('enhancer function to get an enhancer', () => {
     it('should be a function', () => {
-      assert.isFunction(Erux.enhancer);
+      assert.isFunction(enhancer);
     });
     it('should take the correct number of arguments', () => {
-      assert.strictEqual(Erux.enhancer.length, 1);
+      assert.strictEqual(enhancer.length, 1);
     });
     it('should return a function', () => {
-      assert.isFunction(Erux.enhancer());
+      assert.isFunction(enhancer());
     });
     it('should return a function taking the correct number of arguments', () => {
-      assert.strictEqual(Erux.enhancer().length, 3);
+      assert.strictEqual(enhancer().length, 3);
     });
     it('should fail without proper reducer', () => {
-      const enhancer = Erux.enhancer();
-      assert.throws(enhancer, reducerError);
-      assert.throws(() => enhancer(''), reducerError);
-      assert.throws(() => enhancer([]), reducerError);
-      assert.throws(() => enhancer({}), reducerError);
+      assert.throws(enhancer(), reducerError);
+      assert.throws(() => enhancer()(''), reducerError);
+      assert.throws(() => enhancer()([]), reducerError);
+      assert.throws(() => enhancer()({}), reducerError);
     });
   });
 
   describe('on function to add a reducer for an action type', () => {
     it('should be a function', () => {
-      assert.isFunction(Erux.on);
+      assert.isFunction(on);
     });
     it('should take the correct number of arguments', () => {
-      assert.strictEqual(Erux.on.length, 2);
+      assert.strictEqual(on.length, 2);
     });
     it('should fail without type', () => {
       const typeError = 'Type is required';
-      assert.throws(Erux.on, typeError);
-      assert.throws(() => Erux.on(''), typeError);
-      assert.throws(() => Erux.on([]), typeError);
-      assert.throws(() => Erux.on({}), typeError);
+      assert.throws(on, typeError);
+      assert.throws(() => on(''), typeError);
+      assert.throws(() => on([]), typeError);
+      assert.throws(() => on({}), typeError);
     });
     it('should fail without reducer', () => {
-      assert.throws(() => Erux.on(' '), reducerError);
+      assert.throws(() => on(' '), reducerError);
     });
     describe('when called with a simple number reducer', () => {
       const PLUS_ONE = 'PLUS_ONE';
-      Erux.on(PLUS_ONE, state => state + 1);
+      on(PLUS_ONE, state => state + 1);
       it('should result in a correct reducer being available', () => {
-        const reducedState = Erux.reducer(0, { type: PLUS_ONE });
+        const reducedState = reducer(0, { type: PLUS_ONE });
         assert.strictEqual(reducedState, 1);
       });
     });
     describe('when called with a simple string reducer', () => {
       const CONCAT = 'CONCAT';
-      Erux.on(CONCAT, state => state.concat('cat'));
+      on(CONCAT, state => state.concat('cat'));
       it('should result in a correct reducer being available', () => {
-        const reducedState = Erux.reducer('', { type: CONCAT });
+        const reducedState = reducer('', { type: CONCAT });
         assert.strictEqual(reducedState, 'cat');
       });
     });
@@ -78,16 +78,19 @@ describe('Erux', () => {
         ...state,
         loading: false,
         loaded: true,
-        request: action.payload,
+        request: action.pageToLoad,
       });
-      Erux.on(LOAD, loadReducer);
+      const initialState = { loading: true };
+      const loadAction = { type: LOAD, pageToLoad };
+      const expectedReducedState = {
+        loading: false,
+        loaded: true,
+        request: pageToLoad,
+      };
+      on(LOAD, loadReducer);
       it('should result in a correct reducer being available', () => {
-        const reducedState = Erux.reducer({ loading: true }, { type: LOAD, payload: pageToLoad });
-        assert.deepEqual(reducedState, {
-          loading: false,
-          loaded: true,
-          request: pageToLoad,
-        });
+        const reducedState = reducer(initialState, loadAction);
+        assert.deepEqual(reducedState, expectedReducedState);
       });
     });
   });
